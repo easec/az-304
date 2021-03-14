8eea10bbcf35828feb3b5fa55bf0d9f541b8d3b2
easec <mats.johannesson@easec.se>
2021-03-14 19:19:22 +0100

    Update terraform_azure.tf

--- Modul_1/terraform_azure.tf
+++ Modul_1/terraform_azure.tf
@@ -55,29 +55,17 @@ resource "azurerm_network_security_group" "myterraformnsg" {
     resource_group_name = azurerm_resource_group.myterraformgroup.name
 
     security_rule {
-        name                       = "SSH"
+        name                       = "SSH&http"
         priority                   = 1001
         direction                  = "Inbound"
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "22"
+        destination_port_range     = "22, 80"
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
     
-        security_rule {
-        name                       = "http"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "80"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
     tags = {
         environment = ".easec Terraform Demo"
     }
